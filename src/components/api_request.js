f498d24919ebe88bf043690c8c7512f2ef3489e6
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-01 22:39:31 +0900

    signIn 함수 변경 취소

--- src/components/api_request.js
+++ src/components/api_request.js
@@ -4,7 +4,7 @@ let $http;
 const pRequest = (url, data, cb, fcb, ecb) => $http.post(baseURL + url, data).then(r => r.success ? cb(r) : fcb(r)).catch(e => ecb(e));
 export default {
   set$http: http => $http = http,
-  signIn: (data, cb) => pRequest('/account/t/login', data, result =>
+  signIn: (email, password, cb) => pRequest('/account/t/login', {email: email, password: password}, result =>
     cb(result.data.message.session, result.data.message.name), () => alert('잘못된 ID 또는 패스워드입니다.'), e => alert(e)),
   addStudent: (data, cb) => pRequest('/account/manage/student', data, cb, () => alert('로드 실패'),
     e => alert(e))
