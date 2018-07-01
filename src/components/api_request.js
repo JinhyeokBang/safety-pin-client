09888061e4ce6bfe0e70f91373c3f2b9e6e00d22
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-01 22:38:06 +0900

    baseURI를 baseURL로, addStudent 메소드 추가, signIn 규격 변경

--- src/components/api_request.js
+++ src/components/api_request.js
@@ -1,10 +1,11 @@
 /** API 변경시 변경 **/
-const baseURI = 'https://letscoding.kr:8888/api/v1';
+const baseURL = 'https://letscoding.kr:8888/api/v1';
 let $http;
-const postRequest = (url, data, cb, fcb, ecb) => $http.post(url, data).then(r => r.success ? cb(r) : fcb(r)).catch(e => ecb(e));
+const pRequest = (url, data, cb, fcb, ecb) => $http.post(baseURL + url, data).then(r => r.success ? cb(r) : fcb(r)).catch(e => ecb(e));
 export default {
   set$http: http => $http = http,
-  signIn: (email, password, callback) => postRequest(`${baseURI}/account/t/login`, {email: email, password: password},
-    result => callback(result.data.message.session, result.data.message.name), () => alert('잘못된 ID 또는 패스워드입니다.'), e => alert(e))
-
+  signIn: (data, cb) => pRequest('/account/t/login', data, result =>
+    cb(result.data.message.session, result.data.message.name), () => alert('잘못된 ID 또는 패스워드입니다.'), e => alert(e)),
+  addStudent: (data, cb) => pRequest('/account/manage/student', data, cb, () => alert('로드 실패'),
+    e => alert(e))
 };
