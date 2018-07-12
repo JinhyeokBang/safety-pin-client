3bd940b23567ffe2d5e102bfa0c5b42495167e0c
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-12 17:23:56 +0900

    css라이브러리에서 스크롤이 기본으로 되어있는 오류를 덮어씌움

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,23 @@
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data () {
-    return {
-      appName: "safety-pin"
+  export default {
+    name: 'App',
+    data() {
+      return {
+        appName: "safety-pin"
+      }
     }
   }
-}
 </script>
 
 <style>
 
   * {
     font-family: 'Nanum Gothic', sans-serif;
+  }
+
+  html {
+    overflow: auto !important;
   }
 </style>
