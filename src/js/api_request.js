f4e9507df922a45b17fc84adea086c4c769f8214
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-14 23:24:25 +0900

    로그인시 오류가 발생하는 부분을 수정

--- src/js/api_request.js
+++ src/js/api_request.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const baseURL = 'https://letscoding.kr:8888/api/v1';
 const pRequest = (url, data, cb, fcb, ecb) => axios.post(baseURL + url, data).then(r => r.success ? cb(r) : fcb(r)).catch(e => ecb(e));
 export default {
-  signIn: (email, password, cb) => pRequest('/account/t/login', {email: email, password: password}, result =>
-    cb(result.data.message.session, result.data.message.name), () => alert('잘못된 ID 또는 패스워드입니다.'), e => alert(e)),
+  signIn: (email, password, cb) => pRequest('/account/t/login', {email, password}, result =>
+    cb(result.data.message.session, result.data.message.name), cb,r => alert('잘못된 ID 또는 패스워드입니다.'), e => alert(e)),
   addStudent: (data, cb) => pRequest('/account/manage/student', data, cb, () => alert('로드 실패'),
     e => alert(e)),
   signUp: (email, password, name, className, info, cb) => pRequest('/account/t/register', {
