34c5b73f63dde54d29374b3b28d27011ff0cbca1
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-15 01:34:04 +0900

    loadChat 메소드 추가

--- src/js/api_request.js
+++ src/js/api_request.js
@@ -6,9 +6,10 @@ const pRequest = (url, data, cb, fcb) => axios.post(baseURL + url, data).then(r 
 export default {
   signIn: (email, password, cb) => pRequest('/account/t/login', {email, password}, result =>
     cb(result.message.session, result.message.name), cb, () => alert('잘못된 ID 또는 패스워드입니다.')),
-  addStudent: (data, cb) => pRequest('/manage/student', data, cb, () => console.log('학생을 추가하는데 실패했습니다.')),
-  loadStudent: (data, cb) => pRequest('/account/t/load', data, cb, () => console.log('학생목록을 불러오는데 실패했습니다.')),
   signUp: (email, password, name, className, info, cb) => pRequest('/account/t/register', {
     name, email, password, 'class': className, info
-  }, cb, () => alert('회원가입에 실패하였습니다.'))
+  }, cb, () => alert('회원가입에 실패하였습니다.')),
+  addStudent: (data, cb) => pRequest('/manage/student', data, cb, () => console.log('학생을 추가하는데 실패했습니다.')),
+  loadStudent: (data, cb) => pRequest('/account/t/load', data, cb, () => console.log('학생목록을 불러오는데 실패했습니다.')),
+  loadChat: (data, cb) => pRequest('/chat/t/load', data, cb, () => console.log('채팅목록을 불러오는데 실패했습니다.'))
 };
