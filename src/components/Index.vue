2410e947e2bb340d0817ebb74455d44059a5f99e
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-15 00:54:20 +0900

    교사가 학생추가시 더 이상 리프레시 되지 않음

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,74 +1,75 @@
 <template>
-    <v-app>
-        <section class="main-container">
-            <sideMenu :name="name"></sideMenu>
-            <section class="content" style="background-color:#2682FF;">
-                <v-layout column="">
-                    <v-jumbotron style="background-color:#2682FF;" v-if="!isLogin">
-                        <v-container fill-height>
-                            <v-layout align-center>
-                                <v-flex class="fw">
-                                    <section class="logo">
-                                        <img src="./../assets/icon.png" alt="" srcset="">
-                                        <h1>Safety PIN</h1>
-                                        <span style="font-size:1.5rem">학교를 위한 보안 솔루션</span>
-                                        <span class="subheading">사이트 이용을 위해 로그인을 해주세요. </span>
-                                        <br>
-                                        <a data-v-331f341c="" href="#/signin" class="mx-0 btn btn--large btn--router" style="color: white;">
-                                            <div class="btn__content" style="border: solid;">Login</div>
-                                        </a>
-                                    </section>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                    </v-jumbotron>
-                    <v-layout row justify-center v-if="isLogin" style="padding-top:25px">
-                        <v-flex sm10 lg6>
-                            <v-card>
-                                <v-list subheader>
-                                    <v-subheader>연락처</v-subheader>
-                                    <v-list-tile v-for="(contact, key) in contacts" :key="key" avatar>
-                                        <v-list-tile-avatar>
-                                            <v-icon>account_circle</v-icon>
-                                        </v-list-tile-avatar>
-                                        <v-list-tile-content>
-                                            <v-list-tile-title
-                                                    v-html="`${contact.name}(${contact.num})  학생코드 : ${contact.code}`"></v-list-tile-title>
-                                        </v-list-tile-content>
-                                        <v-list-tile-action>
-                                            <v-btn v-bind:to="'chat/'+ contact.code" icon>
-                                                <v-icon color="primary">chat_bubble</v-icon>
-                                            </v-btn>
-                                        </v-list-tile-action>
-                                    </v-list-tile>
-                                    <v-list-tile>
-                                        <v-list-tile-content style="width:33%">
-                                            <v-text-field v-model="st_name" type="text" placeholder="추가하실 학생의 이름">
-                                            </v-text-field>
-                                        </v-list-tile-content>
-                                        <v-list-tile-content>
-                                            <v-text-field v-model="st_num" type="text" placeholder="추가하실 학생의 학번">
-                                            </v-text-field>
-                                        </v-list-tile-content>
-                                        <v-list-tile-action style="%" icon>
-                                            <v-btn @click="student_add(st_name, st_num)" icon>
-                                                <v-icon color="green">person_add</v-icon>
-                                            </v-btn>
-                                        </v-list-tile-action>
-                                    </v-list-tile>
-                                </v-list>
-                            </v-card>
-                        </v-flex>
-                    </v-layout>
-                </v-layout>
-            </section>
-        </section>
-    </v-app>
+  <v-app>
+    <section class="main-container">
+      <sideMenu :name="name"></sideMenu>
+      <section class="content" style="background-color:#2682FF;">
+        <v-layout column="">
+          <v-jumbotron style="background-color:#2682FF;" v-if="!isLogin">
+            <v-container fill-height>
+              <v-layout align-center>
+                <v-flex class="fw">
+                  <section class="logo">
+                    <img src="./../assets/icon.png" alt="" srcset="">
+                    <h1>Safety PIN</h1>
+                    <span style="font-size:1.5rem">학교를 위한 보안 솔루션</span>
+                    <span class="subheading">사이트 이용을 위해 로그인을 해주세요. </span>
+                    <br>
+                    <a data-v-331f341c="" href="#/signin" class="mx-0 btn btn--large btn--router" style="color: white;">
+                      <div class="btn__content" style="border: solid;">Login</div>
+                    </a>
+                  </section>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-jumbotron>
+          <v-layout row justify-center v-if="isLogin" style="padding-top:25px">
+            <v-flex sm10 lg6>
+              <v-card>
+                <v-list subheader>
+                  <v-subheader>연락처</v-subheader>
+                  <v-list-tile v-for="(contact, key) in contacts" :key="key" avatar>
+                    <v-list-tile-avatar>
+                      <v-icon>account_circle</v-icon>
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title
+                        v-html="`${contact.name}(${contact.num})  학생코드 : ${contact.code}`"></v-list-tile-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                      <v-btn v-bind:to="'chat/'+ contact.code" icon>
+                        <v-icon color="primary">chat_bubble</v-icon>
+                      </v-btn>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content style="width:33%">
+                      <v-text-field v-model="st_name" type="text" placeholder="추가하실 학생의 이름">
+                      </v-text-field>
+                    </v-list-tile-content>
+                    <v-list-tile-content>
+                      <v-text-field v-model="st_num" type="text" placeholder="추가하실 학생의 학번">
+                      </v-text-field>
+                    </v-list-tile-content>
+                    <v-list-tile-action style="%" icon>
+                      <v-btn @click="student_add()" icon>
+                        <v-icon color="green">person_add</v-icon>
+                      </v-btn>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-layout>
+      </section>
+    </section>
+  </v-app>
 </template>
 
 <script>
   import api_request from "../js/api_request";
   import sideMenu from './SideMenu';
+  import Vue from 'vue';
 
   export default {
     name: 'Index',
@@ -86,8 +87,15 @@
       }
     },
     methods: {
-      student_add(name, number) {
-        api_request.addStudent({session: this.$session.get('session'), name: name, number: number}, () => window.location.reload());
+      student_add() {
+        if (this.st_name && this.st_num) api_request.addStudent({
+          session: this.session, name: this.st_name, number: this.st_num
+        }, r => Vue.set(this.contacts, this.contacts.length, {
+          name: this.st_name,
+          code: r.message['code'],
+          num: this.st_num
+        }));
+        else alert('학번과 이름을 모두 입력해주세요.');
       },
     },
     created() {
@@ -108,40 +116,40 @@
 </script>
 
 <style scoped>
-    .fw {
-        color: white;
-    }
+  .fw {
+    color: white;
+  }
 
-    .main-container {
-        width: 100%;
-        min-height: 100vh;
-        display: flex;
-        flex-direction: row;
-        overflow: hidden;
-    }
+  .main-container {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+  }
 
-    .logo h1 {
-        color: #fff;
-        font-family: 'Open Sans', sans-serif;
-        font-size: 2rem;
-        font-weight: 300;
-    }
+  .logo h1 {
+    color: #fff;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 2rem;
+    font-weight: 300;
+  }
 
-    .logo {
-        height: 220px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-    }
+  .logo {
+    height: 220px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-    .logo img {
-        width: 120px;
-    }
+  .logo img {
+    width: 120px;
+  }
 
-    .nav-container {
-        height: 100vh;
+  .nav-container {
+    height: 100vh;
 
-    }
+  }
 </style>
