286d53aa3a166baf42edda950c5f05f6271d7c23
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-14 23:27:41 +0900

    ChatList 리펙토링

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,31 +1,31 @@
 <template>
-    <v-app>
-        <section class="main-container">
-            <sideMenu :name="name"></sideMenu>
-            <section class="content layout row justify-center">
-                <v-list id="chatContent" style="min-width: 50%">
-                    <div class="chat-container">
-                        <v-list-tile v-for="item in chatMessages" :key="item.timestp" avatar>
-                            <v-list-tile-avatar>
-                                <v-icon>account_circle</v-icon>
-                            </v-list-tile-avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title v-html="item.direction === 0?item.name:'나'"></v-list-tile-title>
-                                <v-list-tile-sub-title v-html="item.chat"></v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </div>
-                    <v-card flat style="padding:10px;">
-                        <v-form @keyup.enter.native="sendChatMessageMethod(chat)">
-                            <v-text-field v-model="chat"></v-text-field>
-                            <v-btn flat color="blue" @click="sendChatMessageMethod(chat)">보내기</v-btn>
-                        </v-form>
-                    </v-card>
-                </v-list>
+  <v-app>
+    <section class="main-container">
+      <sideMenu :name="name"></sideMenu>
+      <section class="content layout row justify-center">
+        <v-list id="chatContent" style="min-width: 50%">
+          <div class="chat-container">
+            <v-list-tile v-for="item in chatMessages" :key="item.timestp" avatar>
+              <v-list-tile-avatar>
+                <v-icon>account_circle</v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.direction === 0?item.name:'나'"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.chat"></v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </div>
+          <v-card flat style="padding:10px;">
+            <v-form @keyup.enter.native="sendChat(chat)">
+              <v-text-field v-model="chat"></v-text-field>
+              <v-btn flat color="blue" @click="sendChat(chat)">보내기</v-btn>
+            </v-form>
+          </v-card>
+        </v-list>
 
-            </section>
-        </section>
-    </v-app>
+      </section>
+    </section>
+  </v-app>
 </template>
 
 <script>
@@ -38,22 +38,19 @@
       getSession() {
         return this.$session.get('session')
       },
-      loadChatMessageMethod() {
+      loadChat() {
         const baseURI = 'https://letscoding.kr:8888/api/v1';
         this.$http.post(`${baseURI}/chat/t/load/`, {
           session: this.session,
           id: this.$route.params.child_id
-        }).then((result) => this.loadChatSuccessed(result.data.message.chat))
-          .catch((err) => alert(err))
+        }).then(result => this.chatMessages = result.data.message.chat)
+          .catch(err => alert(err))
       },
-      loadChatSuccessed(chat) {
-        this.chatMessages = chat
-      },
-      sendChatMessageMethod(chat) {
+      sendChat(chat) {
         const baseURI = 'https://letscoding.kr:8888/api/v1';
         this.$http.post(`${baseURI}/chat/t/send`, {session: this.session, id: this.$route.params.child_id, chat: chat})
-          .then(() => this.loadChatMessageMethod())
-          .catch((err) => alert(err));
+          .then(() => this.loadChat())
+          .catch(() => alert('채팅 전송에 오류가 발생하였습니다.'));
         this.chat = "";
       },
     },
@@ -74,50 +71,48 @@
     created() {
       if (!this.$session.exists()) this.$router.push('/signin');
       this.session = this.getSession();
-      this.loadChatMessageMethod(this.session);
-      setInterval(() => this.loadChatMessageMethod(), 5000);
+      this.loadChat(this.session);
+      setInterval(() => this.loadChat(), 5000);
     }
   }
 </script>
 
 <style scoped>
-    @import '~fullcalendar/dist/fullcalendar.css';
+  .main-container {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+  }
 
-    .main-container {
-        width: 100%;
-        min-height: 100vh;
-        display: flex;
-        flex-direction: row;
-    }
+  .chat-container {
+    height: 90%;
+    overflow-y: scroll;
+  }
 
-    .chat-container {
-        height: 90%;
-        overflow-y: scroll;
-    }
+  .nav-container {
+    height: 100vh;
+    min-width: 6vw;
+  }
 
-    .nav-container {
-        height: 100vh;
-        min-width: 6vw;
-    }
+  .calendar {
+    float: left;
+  }
 
-    .calendar {
-        float: left;
-    }
+  #chatContent {
+    height: 90vh;
+  }
 
-    #chatContent {
-        height: 90vh;
-    }
+  .chat-container::-webkit-scrollbar {
+    width: 10px;
+    background-color: #628994;
+  }
 
-    .chat-container::-webkit-scrollbar {
-        width: 10px;
-        background-color: #628994;
-    }
+  .chat-container::-webkit-scrollbar-thumb {
+    background-color: #628994;
+  }
 
-    .chat-container::-webkit-scrollbar-thumb {
-        background-color: #628994;
-    }
-
-    .chat-container::-webkit-scrollbar-track {
-        background-color: #F5F5F5;
-    }
+  .chat-container::-webkit-scrollbar-track {
+    background-color: #F5F5F5;
+  }
 </style>
