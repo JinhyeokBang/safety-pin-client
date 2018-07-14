c98762448ffe0bbd58b895c41f5d0738bb029ab5
Shin-JaeHeon <heoni0504@gmail.com>
2018-07-15 00:57:19 +0900

    loadStudent 추가

--- src/js/api_request.js
+++ src/js/api_request.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 /** API 변경시 변경 **/
 const baseURL = 'https://letscoding.kr:8888/api/v1';
-const pRequest = (url, data, cb, fcb, ecb) => axios.post(baseURL + url, data).then(r => r.data.success ? cb(r.data) : fcb(r)).catch(e => ecb(e));
+const pRequest = (url, data, cb, fcb) => axios.post(baseURL + url, data).then(r => r.data.success ? cb(r.data) : fcb()).catch(e => alert(e));
 export default {
   signIn: (email, password, cb) => pRequest('/account/t/login', {email, password}, result =>
-    cb(result.message.session, result.message.name), cb, () => alert('잘못된 ID 또는 패스워드입니다.'), e => alert(e)),
-  addStudent: (data, cb) => pRequest('/manage/student', data, cb, r => console.log(r), e => alert(e)),
+    cb(result.message.session, result.message.name), cb, () => alert('잘못된 ID 또는 패스워드입니다.')),
+  addStudent: (data, cb) => pRequest('/manage/student', data, cb, () => console.log('학생을 추가하는데 실패했습니다.')),
+  loadStudent: (data, cb) => pRequest('account/t/load', data, cb, () => console.log('학생목록을 불러오는데 실패했습니다.')),
   signUp: (email, password, name, className, info, cb) => pRequest('/account/t/register', {
     name, email, password, 'class': className, info
-  }, cb, () => alert('회원가입에 실패하였습니다.'), e => alert(e))
+  }, cb, () => alert('회원가입에 실패하였습니다.'))
 };
